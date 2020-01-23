53dd99a3a5fec992160bc646ab507ec175617c30
gopisingh888 <officialgopisingh@gmail.com>
2020-01-23 15:25:55 +0530

    create inputform & minor fixes

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,41 +2,41 @@ import React from 'react';
 import Propcard from './Propcard';
 
 const Card = () => (
-    <div className="has-background-light">
-  <div className="container section ">
-    <div className="columns">
-      <div className="column">
-        <Propcard
-          img="https://colorlib.com/preview/theme/carries/img/blog/blog_1.jpg"
-          largetext="OCEAN FREIGHT"
-          smalltext="It is a long established fact 
+  <div className="has-background-light">
+    <div className="container section ">
+      <div className="columns">
+        <div className="column">
+          <Propcard
+            img="https://colorlib.com/preview/theme/carries/img/blog/blog_1.jpg"
+            largetext="OCEAN FREIGHT"
+            smalltext="It is a long established fact
           that a reader will be distracted by the readable content of a page when looking at its layout.."
-          icon="fas fa-ship"
-          button="readmore"
-        />
-      </div>
-      <div className="column">
-        <Propcard
-          img="https://colorlib.com/preview/theme/carries/img/blog/blog_2.jpg"
-          largetext="AIR FREIGHT"
-          smalltext="It is a long established fact 
+            icon="fas fa-ship"
+            button="readmore"
+          />
+        </div>
+        <div className="column">
+          <Propcard
+            img="https://colorlib.com/preview/theme/carries/img/blog/blog_2.jpg"
+            largetext="AIR FREIGHT"
+            smalltext="It is a long established fact
           that a reader will be distracted by the readable content of a page when looking at its layout.."
-          icon="fas fa-plane"
-          button="readmore"
-        />
-      </div>
-      <div className="column">
-        <Propcard
-          img="https://colorlib.com/preview/theme/carries/img/blog/blog_3.jpg"
-          largetext="STREET FREIGHT"
-          smalltext="It is a long established fact 
+            icon="fas fa-plane"
+            button="readmore"
+          />
+        </div>
+        <div className="column">
+          <Propcard
+            img="https://colorlib.com/preview/theme/carries/img/blog/blog_3.jpg"
+            largetext="STREET FREIGHT"
+            smalltext="It is a long established fact
           that a reader will be distracted by the readable content of a page when looking at its layout.."
-          icon="fas fa-truck"
-          button="readmore"
-        />
+            icon="fas fa-truck"
+            button="readmore"
+          />
+        </div>
       </div>
     </div>
-  </div>
   </div>
 );
 export default Card;
--- src/components/Inputform.js
+++ src/components/Inputform.js
@@ -1,24 +1,92 @@
 import React from 'react';
 import styled from 'styled-components';
-const Form = styled.p`
-p{
-color:${props => props.theme.greyshades};
-}
+
+const Forms = styled.p`
+  p {
+    color: ${props => props.theme.greyshade} !important ;
+  }
+  .is-medium {
+    margin-bottom: 10px;
+  }
 `;
+
 const Inputform = () => (
-<div className="columns">
-    <Form>
-        <div className="column is-two-fifths">
-           
-            <div className="box has-background-grey-lighter">
-            <p>GET A QUOTE</p>
-            <input class="input" type="text" placeholder="Text input" />
-                <div className=""></div>
+  <div className="container">
+    <Forms>
+      <div className="columns is-gap is-8">
+        <div className="column is-one-third">
+          <div className="box width-full has-background-grey-lighter ">
+            <p className="is-size-3 title">GET A QUOTE</p>
+            <p className="width-full">
+              <input
+                className="input is-medium	"
+                type="text"
+                name="fullname"
+                id="name"
+                placeholder="Your Name"
+              />
+            </p>
+            <div className="field is-horizontal">
+              <div className="field-body">
+                <div className="field">
+                  <p className="control is-expanded has-icons-left">
+                    <input className="input" type="text" placeholder="Name" />
+                  </p>
+                </div>
+                <div className="field">
+                  <p className="control is-expanded has-icons-left has-icons-right">
+                    <input className="input" type="email" placeholder="Email" />
+                  </p>
+                </div>
+              </div>
             </div>
+            <div className="field is-horizontal">
+              <div className="field-body">
+                <div className="field">
+                  <p className="control is-expanded has-icons-left">
+                    <input className="input" type="text" placeholder="Name" />
+                  </p>
+                </div>
+                <div className="field">
+                  <p className="control is-expanded has-icons-left has-icons-right">
+                    <input className="input" type="email" placeholder="Email" />
+                  </p>
+                </div>
+              </div>
+            </div>
+            <p className="width-full">
+              <input
+                className="input is-medium	"
+                type="text"
+                name="address"
+                id="address"
+                placeholder="Destination"
+              />
+            </p>
+            <div className="field is-horizontal">
+              <div className="field-body">
+                <div className="field">
+                  <div className="control">
+                    <textarea
+                      className="textarea"
+                      placeholder="Explain how we can help you"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="button is-secondary is-medium">Send</div>
+          </div>
         </div>
         <div className="column">
+          <p className="title">WE HAVE 25 YEARS EXPERIENCE IN THIS PASSION</p>
+          <p>There are many variations of passages of Lorem Ips
+          um available, but the majority have suffered alteration in some form,
+           by injected humour, or randomised words which don't look even slightly believable.
+           If you are going to use a passage of Lorem Ipsum</p>
         </div>
-    </Form>    
-</div>
+      </div>
+    </Forms>
+  </div>
 );
 export default Inputform;
