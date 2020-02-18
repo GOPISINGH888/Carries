a271fe28baa0f87aaad7fdf7c6793e491b9d73f4
gopisingh888 <officialgopisingh@gmail.com>
2020-02-18 15:11:56 +0530

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,158 +1,170 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Fooerstyle = styled.div`
+  .title {
+    margin-bottom: 1rem;
+    color: ${props => props.theme.mainbrandcolor} !important;
+  }
+  .dotedlines {
+    color: ${props => props.theme.darkAccent} !important;
+  }
+`;
 const Footer = () => (
   <div className="container">
-    <div className="columns">
-      <div className="column">
-        <div className="title">About Us</div>
-        <p>--------</p>
-        <p className="aboutext">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit
-        </p>
-        <div>
-          <i className="fas fa-phone-alt" />
-          <p className="contactno."> +8801911854378</p>
-        </div>
-        <div>
-          <i className="fas fa-map-marker-alt" />
-          <p className="email">backpiper.com@gmail.com</p>
-        </div>
-        <div>
-          <i className="fas fa-phone-alt" />
-          <p className="address"> Gazipur, 1704, Bangladesh. 1215.</p>
-        </div>
-      </div>
-      <div className="column">
-        <div className="title">Latest Tweets</div>
-        <p>--------</p>
-        <div>
-          <p className="tweetext">
-            {' '}
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem .
+    <Footerstyle>
+      <div className="columns">
+        <div className="column">
+          <div className="title">About Us</div>
+          <p className="dotedlines">--------</p>
+          <p className="aboutext">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
           </p>
-          <p className="munitsago">5 Miniutes Ago</p>
-          <p className="tweetext">
-            {' '}
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem .
-          </p>
-          <p className="munitsago">5 Miniutes Ago</p>
+          <div>
+            <i className="fas fa-phone-alt" />
+            <p className="contactno."> +8801911854378</p>
+          </div>
+          <div>
+            <i className="fas fa-map-marker-alt" />
+            <p className="email">backpiper.com@gmail.com</p>
+          </div>
+          <div>
+            <i className="fas fa-phone-alt" />
+            <p className="address"> Gazipur, 1704, Bangladesh. 1215.</p>
+          </div>
         </div>
-      </div>
-      <div className="column">
-        <div className="title">Customer Service</div>
-        <p>--------</p>
-        <div className="breadcrumb" aria-label="breadcrumbs">
-          <ul className="is-grid">
-            <li>
-              <a href="#">
-                <span className="icon is-small">
-                  <i className="far fa-hand-point-right" />
-                </span>
-                <span>Support Forums</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span className="icon is-small">
-                  <i className="far fa-hand-point-right" />
-                </span>
-                <span>Communication</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span className="icon is-small">
-                  <i className="far fa-hand-point-right" />
-                </span>
-                <span>Contact Us</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span className="icon is-small">
-                  <i className="far fa-hand-point-right" />
-                </span>
-                <span>Privacy Policy</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span className="icon is-small">
-                  <i className="far fa-hand-point-right" />
-                </span>
-                <span>Rules & Condition</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span className="icon is-small">
-                  <i className="far fa-hand-point-right" />
-                </span>
-                <span>FAQ</span>
-              </a>
-            </li>
-          </ul>
+        <div className="column">
+          <div className="title">Latest Tweets</div>
+          <p className="dotedlines">--------</p>
+          <div>
+            <p className="tweetext">
+              {' '}
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem .
+            </p>
+            <p className="munitsago">5 Miniutes Ago</p>
+            <p className="tweetext">
+              {' '}
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem .
+            </p>
+            <p className="munitsago">5 Miniutes Ago</p>
+          </div>
         </div>
-      </div>
-      <div className="column">
-        <div className="imggrid">
+        <div className="column">
           <div className="title">Customer Service</div>
-          <p>--------</p>
-          <ul className="is-inline-flex">
-            <li>
-              <a href="#">
-                <img
-                  src="https://colorlib.com/preview/theme/carries/img/instafeed/1.jpg"
-                  alt=""
-                />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <img
-                  src="https://colorlib.com/preview/theme/carries/img/instafeed/2.jpg"
-                  alt=""
-                />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <img
-                  src="https://colorlib.com/preview/theme/carries/img/instafeed/3.jpg"
-                  alt=""
-                />
-              </a>
-            </li>
-          </ul>
-          <ul className="is-inline-flex">
-            <li>
-              <a href="#">
-                <img
-                  src="https://colorlib.com/preview/theme/carries/img/instafeed/4.jpg"
-                  alt=""
-                />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <img
-                  src="https://colorlib.com/preview/theme/carries/img/instafeed/5.jpg"
-                  alt=""
-                />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <img
-                  src="https://colorlib.com/preview/theme/carries/img/instafeed/6.jpg"
-                  alt=""
-                />
-              </a>
-            </li>
-          </ul>
+          <p className="dotedlines">--------</p>
+          <div className="" aria-label="breadcrumbs">
+            <ul className="is-grid">
+              <li>
+                <a href="#">
+                  <span className="icon is-small">
+                    <i className="far fa-hand-point-right" />
+                  </span>
+                  <span>Support Forums</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span className="icon is-small">
+                    <i className="far fa-hand-point-right" />
+                  </span>
+                  <span>Communication</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span className="icon is-small">
+                    <i className="far fa-hand-point-right" />
+                  </span>
+                  <span>Contact Us</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span className="icon is-small">
+                    <i className="far fa-hand-point-right" />
+                  </span>
+                  <span>Privacy Policy</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span className="icon is-small">
+                    <i className="far fa-hand-point-right" />
+                  </span>
+                  <span>Rules & Condition</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span className="icon is-small">
+                    <i className="far fa-hand-point-right" />
+                  </span>
+                  <span>FAQ</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="column">
+          <div className="imggrid">
+            <div className="title">Customer Service</div>
+            <p className="dotedlines">--------</p>
+            <ul className="is-inline-flex">
+              <li>
+                <a href="#">
+                  <img
+                    src="https://colorlib.com/preview/theme/carries/img/instafeed/1.jpg"
+                    alt=""
+                  />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img
+                    src="https://colorlib.com/preview/theme/carries/img/instafeed/2.jpg"
+                    alt=""
+                  />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img
+                    src="https://colorlib.com/preview/theme/carries/img/instafeed/3.jpg"
+                    alt=""
+                  />
+                </a>
+              </li>
+            </ul>
+            <ul className="is-inline-flex">
+              <li>
+                <a href="#">
+                  <img
+                    src="https://colorlib.com/preview/theme/carries/img/instafeed/4.jpg"
+                    alt=""
+                  />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img
+                    src="https://colorlib.com/preview/theme/carries/img/instafeed/5.jpg"
+                    alt=""
+                  />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img
+                    src="https://colorlib.com/preview/theme/carries/img/instafeed/6.jpg"
+                    alt=""
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </Footerstyle>
   </div>
 );
 export default Footer;
